68a7ed752a79a303b869550a0f018562df93fa24
parkjungkwan <pakjkwan@gmail.com>
2020-12-07 17:00:22 +0900

    17

--- src/main/java/com/example/demo/controllers/ArticleController.java
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.example.demo.utils.Printer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArticleController{
     @Autowired Printer printer;
     
+    @PostMapping("/articles")
+    public Map<?,?> write(){
+        var map = new HashMap<>();
+        return map;
+    }
     
 }
 
